9b3fed06bd00df4739a4d3a38e1c3d8aec6720e2
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-07 13:25:53 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const textbox = document.getElementById('ranter');
 const button = document.getElementById('ranter_sender');
 button.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://cs-api.pltw.org/question?text=test");
+  xhr.open("POST", "https://cs-api.pltw.org/rant?text=".concat(encodeURIComponent(textbox.value));
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
@@ -11,6 +11,18 @@ button.addEventListener('click', () => {
       console.log(data);
     } else {
       console.log(`Error: ${xhr.status}`);
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://cs-api.pltw.org/newuser/rant");
+        xhr.send();
+        xhr.responseType = "json";
+        xhr.onload = () => {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            const data = xhr.response;
+            console.log(data);
+          } else {
+            console.log(`Error: ${xhr.status}`);
+          }
+        };
     }
   };
 });
