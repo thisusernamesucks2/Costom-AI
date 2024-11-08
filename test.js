efb581b24eeffea44c2f43724c56ff5ca4225d9f
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-08 09:02:22 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const textbox = document.getElementById('ranter');
 const button = document.getElementById('ranter_sender');
 button.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://cs-api.pltw.org/rant?text=".concat(encodeURIComponent(textbox.value)));
+  xhr.open("POST", "https://cs-api.pltw.org/rant?text=" +encodeURIComponent(textbox.value));
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
