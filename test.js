dfa4f444a3431b52ce65ef517a8214929519cec7
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-08 10:25:29 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,7 @@ const textbox = document.getElementById('ranter');
 const button = document.getElementById('ranter_sender');
 button.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
-  try{
-    const response = await fetch('https://cors-anywhere.herokuapp.com/https://cs-api.pltw.org/rant?text=${textbox.value}', {
-      method: 'POST',
-      headers: 
-        { 'Content-Type': 'application/json' }
-    }):
-  }
-  xhr.open("POST", "https://cs-api.pltw.org/rant?text=" +encodeURIComponent(textbox.value));
+  xhr.open("POST", "https://cs-api.pltw.org/rant?text=" + encodeURIComponent(textbox.value));
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
