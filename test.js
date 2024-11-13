6c351db34a19076aa04f53f1cf89d629cb4143e3
thisusernamesucks2 <155395917+thisusernamesucks2@users.noreply.github.com>
2024-11-13 07:52:02 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,23 @@
 const textbox = document.getElementById('ranter');
 const button = document.getElementById('ranter_sender');
 button.addEventListener('click', () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://cs-api.pltw.org/rant?text=" + encodeURIComponent(textbox.value));
-  xhr.send();
-  xhr.responseType = "json";
-  xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      const data = xhr.response;
-      console.log(data);
-    } else {
-      console.log(`Error: ${xhr.status}`);
+  const url = "https://cs-api.pltw.org/rant"
+  const fallback = "https://cs-api.pltw.org/newuser/rant"
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "text": textbox.value
+      },
+    });
+    if (response.status !== 200) {
+      const fallresponse = await fetch(fallback, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
     }
-  };
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://cs-api.pltw.org/newuser/rant");
-  xhr.send();
-  xhr.responseType = "json";
-  xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      const data = xhr.response;
-      console.log(data);
-    } else {
-      console.log(`Error: ${xhr.status}`);
-    }
-  };
+  }
 });
